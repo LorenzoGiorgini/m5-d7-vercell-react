f577a144470576a0fb98ece78d1cca2392a38208
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-10-26 15:40:56 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Home from './views/Home'
 import AddProduct from './views/AddProduct'
+import NavBar from './components/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
       <Router>
+        <NavBar/>
         <Route path="/" exact component={Home}/>
         <Route path="/AddProduct" exact component={AddProduct}/>
       </Router>
